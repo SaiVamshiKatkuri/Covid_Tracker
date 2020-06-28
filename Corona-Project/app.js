f5363c985ea9f7cbccf8c2c9a3c491130c40a229
SaiVamshiKatkuri <64994509+SaiVamshiKatkuri@users.noreply.github.com>
2020-06-28 09:27:01 +0530

    Add files via upload

--- Corona-Project/app.js
+++ Corona-Project/app.js
@@ -10,56 +10,10 @@ const api = require('novelcovid');
 
 // you can choose which URL to use, this will not change the behaviour of the API
 
-
+//Variable Declaration//
 const app=express();
-app.use(express.static("public"));
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true}));
-api.settings({
-    baseUrl: 'https://disease.sh' | 'https://api.caw.sh' | 'https://corona.lmao.ninja'
-})
-mongoose.connect("mongodb://localhost:27017/coronausersDB", {useNewUrlParser: true ,useUnifiedTopology: true});
-
-app.get("/",function(req,res){
-const url="https://api.covid19api.com/summary";
-
-request({
-url:"https://api.covid19api.com/summary",
-method:'GET',
-json:true},function(err,response){
-  var Total=response.body;
-  var confirmed=Total.Global.TotalConfirmed;
-  // console.log(confirmed);
-  var recovered=Total.Global.TotalRecovered;
-  var deaths=Total.Global.TotalDeaths;
-  var nconfirmed=Total.Global.NewConfirmed;
-  t = new Date();
-         yesterday = new Date(t.setDate(t.getDate() - 14));
-             console.log(yesterday.toISOString().substring(0,10));
-
-  // console.log(nconfirmed);
-  var nrecovered=Total.Global.NewRecovered;
-  console.log(nrecovered);
-  var ndeaths=Total.Global.NewDeaths;
-  res.render("main", {TConfirmed: confirmed,TRecovered: recovered, TDeaths: deaths, Newconfirmed:nconfirmed,Newrecovered:nrecovered,Newdeaths:ndeaths});
-}
-);
-
-// https.get(url,function(response){
-//
-//   [response].on("data",function(data){
-// // console.log(data);
-// // var Total=[];
-//   var  Total=JSON.parse(data);
-//    console.log(Total.Global);
-// });
-// });
-});
-
-app.post("/",function(req,res){
-var s=req.body.search;
-
-// var flagUrl;
+var flagUrl="";
+var s="";
 var today_cases;
 var today_recovered;
 var total_deaths;
@@ -70,35 +24,79 @@ var total_tests;
 var active_million;
 var test_million;
 
+var Total;
+var Tot=[];
+var Confirm=[];
+var D=[];
+var De=[];
+var Ac=[];
+var L="";
+var today="";
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+api.settings({
+    baseUrl: 'https://disease.sh' | 'https://api.caw.sh' | 'https://corona.lmao.ninja'
+})
+mongoose.connect("mongodb://localhost:27017/coronausersDB", {useNewUrlParser: true ,useUnifiedTopology: true});
+
+app.get("/",function(req,res){
+const url="https://api.covid19api.com/summary";
+request({
+url:"https://api.covid19api.com/summary",
+method:'GET',
+json:true},function(err,response){
+ Total=response.body;
+  var confirmed=Total.Global.TotalConfirmed;
+  var recovered=Total.Global.TotalRecovered;
+  var deaths=Total.Global.TotalDeaths;
+  var nconfirmed=Total.Global.NewConfirmed;
+  t = new Date();
+         yesterday = new Date(t.setDate(t.getDate() - 14));
+  var nrecovered=Total.Global.NewRecovered;
+  var ndeaths=Total.Global.NewDeaths;
+  res.render("main", {TConfirmed: confirmed,TRecovered: recovered, TDeaths: deaths, Newconfirmed:nconfirmed,Newrecovered:nrecovered,Newdeaths:ndeaths});
+}
+);
+});
+// https.get(url,function(response){
+//
+//   [response].on("data",function(data){
+// // console.log(data);
+// // var Total=[];
+//   var  Total=JSON.parse(data);
+//    console.log(Total.Global);
+// });
+// });
+
+
+app.post("/",function(req,res){
+ s=req.body.search;
   api.countries({country:s}).then((element)=>{
-    flagUrl=element.countryInfo.flag;
-  });
-
-   let today = new Date().toISOString().substring(0, 10);
+  flagUrl=element.countryInfo.flag;
+        });
+       console.log(flagUrl);
+    today = new Date().toISOString().substring(0, 10);
     t = new Date();
-      yesterday = new Date(t.setDate(t.getDate() - 45));
+    yesterday = new Date(t.setDate(t.getDate() - 45));
 
-  var Total;
-  var Confirm=[];
-  var D=[];
-  var De=[];
-  var Ac=[];
-  var L;
     request({
     url:"https://api.covid19api.com/country/"+s+"?from=2020-05-10T00:00:00Z&to="+today+"T00:00:00Z",
     method:'GET',
     json:true},function(err,response){
-   Total=response.body;
-               Total.forEach(function(item){
+   Tot=response.body;
+               Tot.forEach(function(item){
                 D.push(item.Date);
 
              Confirm.push(item.Confirmed);
              De.push(item.Deaths);
              Ac.push(item.Active);
                 });
-  L=D.length;
-  res.render('index',{Confirmed:Confirm,Dates:L,Deaths:De,Active:Ac});
-    });
+
+});
+     // L=D.length;
+  res.render('index',{Confirmed:Confirm,Dates:D.length,Deaths:De,Active:Ac});
 });
 
 
